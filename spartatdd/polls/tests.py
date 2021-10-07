bc20e4a0ca6302aa89b4c54e0b36b0d712b7e6c7
Dayeon1351 <zzzz1351@gmail.com>
2021-10-07 14:40:21 +0900

    Add default django model view template

--- spartatdd/polls/tests.py
+++ spartatdd/polls/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get("/polls/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "등록된 설문조사가 없습니다.")
+        self.assertQuerysetEqual(response.context['question_list'], [])
--- spartatdd/polls/views.py
+++ spartatdd/polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Question
 
-# Create your views here.
+
+def index(request):
+    question_list = Question.objects.all()
+    context = {'question_list': question_list}
+
+    return render(request, 'polls/index.html', context)
